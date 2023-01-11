28b401e875e3b4e10a97cc6f0164e12bfa2c0b2d
Дмитро Ворона <dmytro.vorona@nure.ua>
2023-01-11 16:25:08 +0200

    create worker

---
+++ Models/Worker.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestAsignment.Models
+{
+    public class Worker
+    {
+        public string Name { get; set; }
+        public string Position { get; set; }
+        public string Number { get; set; }
+    }
+}
