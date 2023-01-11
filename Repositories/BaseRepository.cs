2f453d9b3dff259fb87c6d341335a89793dd8315
Дмитро Ворона <dmytro.vorona@nure.ua>
2023-01-11 16:42:04 +0200

    Create BaseRepository

---
+++ Repositories/BaseRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TestAsignment.Models.Base;
+using TestAsignment.Repositories.Interfaces;
+
+namespace TestAsignment.Repositories
+{
+    public class BaseRepository<TDBModel> : IBaseRepository<TDBModel> where TDBModel : BaseModel
+    {
+        private ApplicationContext Context { get; set; }
+    }
+}
