55d04694ad8adbc5969c866717413066ef617bc3
Дмитро Ворона <dmytro.vorona@nure.ua>
2023-01-11 16:25:00 +0200

    create documet

---
+++ Models/Document.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestAsignment.Models
+{
+    public class Document
+    {
+        public Guid CarId { get; set; }
+        public Guid WorkerId { get; set; }
+        public virtual Car Car { get; set; }
+        public virtual Worker Worker { get; set; }
+    }
+}
