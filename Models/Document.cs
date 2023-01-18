d0dfdd91891beda8259b8a68eb2f3e18d0fba635
Дмитро Ворона <dmytro.vorona@nure.ua>
2023-01-18 13:02:13 +0200

    Fix Document n Worker class

--- Models/Document.cs
+++ Models/Document.cs
@@ -2,10 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TestAsignment.Models.Base;
 
 namespace TestAsignment.Models
 {
-    public class Document
+    public class Document : BaseModel
     {
         public Guid CarId { get; set; }
         public Guid WorkerId { get; set; }
--- Models/Worker.cs
+++ Models/Worker.cs
@@ -2,13 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TestAsignment.Models.Base;
 
 namespace TestAsignment.Models
 {
-    public class Worker
+    public class Worker : BaseModel
     {
         public string Name { get; set; }
         public string Position { get; set; }
-        public string Number { get; set; }
+        public string PhoneNumber { get; set; }
     }
 }
