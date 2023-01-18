74c806e7ed73966e621a9ab9a20f88a5d45b4fd9
Дмитро Ворона <dmytro.vorona@nure.ua>
2023-01-18 11:36:15 +0200

    Create ApplicationContext

---
+++ Database/ApplicationContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestAsignment.Models;
+
+namespace TestAsignment.Database
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<Document> Documents { get; set; }
+        public DbSet<Worker> Workers { get; set; }
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+        {
+            Database.EnsureCreated();   
+        }
+
+    }
+}
