c3de9ea65b2fba2cc70ae0f67df62a1965318c96
Дмитро Ворона <dmytro.vorona@nure.ua>
2023-01-19 12:40:06 +0200

    Finish

---
+++ MainControllerTest.cs
@@ -0,0 +1,135 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System.Collections.Generic;
+using System;
+using TestAsignment.Controllers;
+using TestAsignment.Models;
+using TestAsignment.Repositories.Interfaces;
+using TestAsignment.Services.Interface;
+using Xunit;
+
+namespace TestAsignment
+{
+    public class MainControllerTest
+    {
+        [Fact]
+        public void GetDataMessage()
+        {
+            var mockDocs = new Mock<IBaseRepository<Document>>();
+            var mockService = new Mock<IRepairService>();
+            var document = GetDoc();
+            mockDocs.Setup(x => x.GetAll()).Returns(new List<Document> { document });
+
+            // Arrange
+            MainController controller = new MainController(mockService.Object, mockDocs.Object);
+
+            // Act
+            JsonResult result = controller.Get() as JsonResult;
+
+            // Assert
+            Assert.Equal(new List<Document> { document }, result?.Value);
+        }
+
+        [Fact]
+        public void GetNotNull()
+        {
+            var mockDocs = new Mock<IBaseRepository<Document>>();
+            var mockService = new Mock<IRepairService>();
+            mockDocs.Setup(x => x.Create(GetDoc())).Returns(GetDoc());
+
+            // Arrange
+            MainController controller = new MainController(mockService.Object, mockDocs.Object);
+            // Act
+            JsonResult result = controller.Get() as JsonResult;
+            // Assert
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public void PostDataMessage()
+        {
+            var mockDocs = new Mock<IBaseRepository<Document>>();
+            var mockService = new Mock<IRepairService>();
+            mockDocs.Setup(x => x.Create(GetDoc())).Returns(GetDoc());
+
+            // Arrange
+            MainController controller = new MainController(mockService.Object, mockDocs.Object);
+
+            // Act
+            JsonResult result = controller.Post() as JsonResult;
+
+            // Assert
+            Assert.Equal("Work was successfully done", result?.Value);
+        }
+
+        [Fact]
+        public void UpdateDataMessage()
+        {
+            var mockDocs = new Mock<IBaseRepository<Document>>();
+            var mockService = new Mock<IRepairService>();
+            var document = GetDoc();
+
+            mockDocs.Setup(x => x.Get(document.Id)).Returns(document);
+            mockDocs.Setup(x => x.Update(document)).Returns(document);
+
+            // Arrange
+            MainController controller = new MainController(mockService.Object, mockDocs.Object);
+
+            // Act
+            JsonResult result = controller.Put(document) as JsonResult;
+
+            // Assert
+            Assert.Equal($"Update successful {document.Id}", result?.Value);
+        }
+
+        [Fact]
+        public void DeleteDataMessage()
+        {
+            var mockDocs = new Mock<IBaseRepository<Document>>();
+            var mockService = new Mock<IRepairService>();
+            var doc = GetDoc();
+
+            mockDocs.Setup(x => x.Get(doc.Id)).Returns(doc);
+            mockDocs.Setup(x => x.Delete(doc.Id));
+
+            // Arrange
+            MainController controller = new MainController(mockService.Object, mockDocs.Object);
+
+            // Act
+            JsonResult result = controller.Delete(doc.Id) as JsonResult;
+
+            // Assert
+            Assert.Equal("Delete successful", result?.Value);
+        }
+
+        public Document GetDoc()
+        {
+            var mockCars = new Mock<IBaseRepository<Car>>();
+            var mockWorkers = new Mock<IBaseRepository<Worker>>();
+
+            var carId = Guid.NewGuid();
+            var workerId = Guid.NewGuid();
+            mockCars.Setup(x => x.Create(new Car()
+            {
+                Id = carId,
+                Name = "car",
+                Number = "123"
+            }));
+
+            mockWorkers.Setup(x => x.Create(new Worker()
+            {
+                Id = workerId,
+                Name = "worker",
+                Position = "manager",
+                PhoneNumber = "380975555555"
+            }));
+
+            return new Document
+            {
+                Id = Guid.NewGuid(),
+                CarId = carId,
+                WorkerId = workerId
+            };
+        }
+    }
+}
---
+++ RepairServiceTest.cs
@@ -0,0 +1,62 @@
+﻿using Moq;
+using System;
+using TestAsignment.Models;
+using TestAsignment.Repositories.Interfaces;
+using TestAsignment.Services.Interface;
+using Xunit;
+
+namespace TestAsignment
+{
+    public class RepairServiceTest
+    {
+        [Fact]
+        public void WorkSuccessTest()
+        {
+            var serviceMock = new Mock<IRepairService>();
+            var mockCars = new Mock<IBaseRepository<Car>>();
+            var mockWorkers = new Mock<IBaseRepository<Worker>>();
+            var mockDocs = new Mock<IBaseRepository<Document>>();
+            var car = CreateCar(Guid.NewGuid());
+            var worker = CreateWorker(Guid.NewGuid());
+            var doc = CreateDoc(Guid.NewGuid(), worker.Id, car.Id);
+
+            mockCars.Setup(x => x.Create(car)).Returns(car);
+            mockDocs.Setup(x => x.Create(doc)).Returns(doc);
+            mockWorkers.Setup(x => x.Create(worker)).Returns(worker);
+
+            serviceMock.Object.Work();
+
+            serviceMock.Verify(x => x.Work());
+        }
+
+        private Car CreateCar(Guid carId)
+        {
+            return new Car()
+            {
+                Id = carId,
+                Name = "car",
+                Number = "123"
+            };
+        }
+
+        private Worker CreateWorker(Guid workerId)
+        {
+            return new Worker()
+            {
+                Id = workerId,
+                Name = "worker",
+                Position = "manager",
+                PhoneNumber = "380975555555"
+            };
+        }
+        private Document CreateDoc(Guid docId, Guid workerId, Guid carId)
+        {
+            return new Document
+            {
+                Id = docId,
+                CarId = carId,
+                WorkerId = workerId
+            };
+        }
+    }
+}
